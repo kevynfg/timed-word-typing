324b166359267e69aefbcceb05761b78cba8db25
kevyn <kevynredlotus@gmail.com>
2020-08-16 00:41:19 -0300

    - Implementado verificação de input - Computação de pontos - Validação de input

--- src/wordfinder.js
+++ src/wordfinder.js
@@ -1,3 +1,5 @@
+var textInput = document.querySelector('input');
+var labelPontos = document.querySelector('label');
 var modalidades = 
     {
     redes: [
@@ -6,14 +8,35 @@ var modalidades =
     'src/img/instagram.svg',
     'src/img/linkedin.svg',
     'src/img/twitter.svg'
-        ]
+        ],
+    textoRedes: ['instagram', 'twitter', 'linkedin', 'gmail', 'facebook']
     }
 
 var i = 0
-
+var pontos = [];
 const montadorTabela = modalidades.redes.forEach(row => {
     let imgs = document.querySelectorAll('img');  
     imgs[i].src = row
     i++
 })
 
+textInput.addEventListener('keypress', (event) => {
+    const textValue = document.querySelector('input').value;
+    const verificarInput = /[^A-Za-z]/.test(textValue);
+    if (event.key === 'Enter' && textValue.length <= 50 && !verificarInput) {
+        
+        event.preventDefault();
+        // console.log(textValue)
+        // const palavrasRedes = modalidades.textoRedes.filter(palavra => palavra.includes(textValue))
+        const palavraRedes = modalidades.textoRedes.filter(palavra => { 
+             if (palavra == textValue.toLowerCase()) {
+                labelPontos.innerText = `${pontos.length+1}`
+                textInput.value = ''
+                return pontos.push(palavra)
+             } else {
+                textInput.value = ''
+                 console.log('errou')
+             }   
+        }) 
+    }
+})
