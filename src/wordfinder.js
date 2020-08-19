f7aa7bb35267b51d1c7f001a949d74aa7054ed00
kevyn <kevynredlotus@gmail.com>
2020-08-19 17:36:17 -0300

    - Update

--- src/wordfinder.js
+++ src/wordfinder.js
@@ -7,36 +7,41 @@ var modalidades =
     'src/img/gmail.svg',
     'src/img/instagram.svg',
     'src/img/linkedin.svg',
-    'src/img/twitter.svg'
+    'src/img/twitter.svg',
+    'src/img/whatsapp.svg',
+    'src/img/spotify.svg',
+    'src/img/youtube.svg'
         ],
-    textoRedes: ['instagram', 'twitter', 'linkedin', 'gmail', 'facebook']
+    textoRedes: ['instagram', 'twitter', 'linkedin',
+     'gmail', 'facebook', 'whatsapp', 'spotify', 'youtube']
     }
 
 var i = 0
-var pontos = [];
+var pontos = []
 const montadorTabela = modalidades.redes.forEach(row => {
-    let imgs = document.querySelectorAll('img');  
+    let imgs = document.querySelectorAll('img')
     imgs[i].src = row
     i++
 })
 
 textInput.addEventListener('keypress', (event) => {
     const textValue = document.querySelector('input').value;
-    const verificarInput = /[^A-Za-z]/.test(textValue);
+    const verificarInput = /[^A-Za-z]/.test(textValue)
     if (event.key === 'Enter' && textValue.length <= 50 && !verificarInput) {
         
         event.preventDefault();
         // console.log(textValue)
         // const palavrasRedes = modalidades.textoRedes.filter(palavra => palavra.includes(textValue))
         const palavraRedes = modalidades.textoRedes.filter(palavra => { 
-             if (palavra == textValue.toLowerCase()) {
+            if (palavra == textValue.toLowerCase()) {
                 labelPontos.innerText = `${pontos.length+1}`
                 textInput.value = ''
-                return pontos.push(palavra)
+                 return pontos.push(palavra)
              } else {
-                textInput.value = ''
-                 console.log('errou')
+                return textInput.value = ''
+                //  console.log(palavraRedes)
              }   
         }) 
     }
 })
+
