770bed6a759b9c32f125c6f97c6afedb9a63adc3
kevyn <kevynredlotus@gmail.com>
2021-01-22 14:41:22 -0300

    - fixed time complexity

--- src/wordfinder.js
+++ src/wordfinder.js
@@ -1,8 +1,7 @@
 var textInput = document.querySelector('input');
 var labelPontos = document.querySelector('label');
-var modalidades = 
-    {
-    redes: [
+var modalidades = {
+  redes: [
     'src/img/facebook.svg',
     'src/img/gmail.svg',
     'src/img/instagram.svg',
@@ -10,41 +9,44 @@ var modalidades =
     'src/img/twitter.svg',
     'src/img/whatsapp.svg',
     'src/img/spotify.svg',
-    'src/img/youtube.svg'
-        ],
-    textoRedes: ['instagram', 'twitter', 'linkedin',
-     'gmail', 'facebook', 'whatsapp', 'spotify', 'youtube']
-    }
+    'src/img/youtube.svg',
+  ],
+  textoRedes: [
+    'instagram',
+    'twitter',
+    'linkedin',
+    'gmail',
+    'facebook',
+    'whatsapp',
+    'spotify',
+    'youtube',
+  ],
+};
 
-var i = 0
-var pontos = []
-const montadorTabela = modalidades.redes.forEach(row => {
-    let imgs = document.querySelectorAll('img')
-    imgs[i].src = row
-    i++
-})
+var i = 0;
+var pontos = [];
+const montadorTabela = modalidades.redes.forEach((row) => {
+  let imgs = document.querySelectorAll('img');
+  imgs[i].src = row;
+  i++;
+});
 
 textInput.addEventListener('keypress', (event) => {
-    const textValue = document.querySelector('input').value;
-    const verificarInput = /[^A-Za-z]/.test(textValue)
-    if (event.key === 'Enter' && textValue.length <= 50 && !verificarInput) {
-        
-        event.preventDefault();
-        // console.log(textValue)
-        // const palavrasRedes = modalidades.textoRedes.filter(palavra => palavra.includes(textValue))
-        const palavraRedes = modalidades.textoRedes.filter(palavra => { 
-            if(pontos.includes(palavra)) {
-                textInput.value = ''
-             } else if (palavra == textValue.toLowerCase()) {
-                    labelPontos.innerText = `${pontos.length+1}`
-                    pontos.push(palavra)
-                    if(pontos.length == modalidades.textoRedes.length) {
-                        alert('Boa você ganhou um biscoito!')
-                    }
-             } else {
-                 textInput.value = ''
-             } 
-        })  
+  const textValue = document.querySelector('input').value;
+  const verificarInput = /[^A-Za-z]/.test(textValue);
+  if (event.key === 'Enter' && textValue.length <= 50 && !verificarInput) {
+    event.preventDefault();
+    if (modalidades.textoRedes.includes(textValue)) {
+      labelPontos.innerText = `${pontos.length + 1}`;
+      pontos.push(textValue);
+      textInput = '';
+      if (pontos.length == modalidades.textoRedes.length) {
+        alert('Boa você ganhou um biscoito!');
+        return;
+      }
+      return;
+    } else {
+      textInput.value = '';
     }
-})
-
+  }
+});
